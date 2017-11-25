9bda87e4badbffaea55f2ab65beacdfd51373340
Veronica Kim <vero.a.kim@gmail.com>
2017-11-25 20:32:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfied"]
 
 function destructivelyAppendKitten(name) {
-  name.push();
-  return name;
+  kittens.push(name);
+  return kittens;
 }
 
 function destructivelyPrependKitten(name) {
   name.unshift()
   return name;
 }
-
